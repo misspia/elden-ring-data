f30921b6f6d4f264086058b1ec931d743aa35fcb
misspia <pia_leung@hotmail.com>
2025-02-13 14:45:48 -0500

    fix camel casing when input is all caps

--- src/types/camelCase.ts
+++ src/types/camelCase.ts
@@ -1,10 +1,23 @@
-export type SentenceToCamelCase<S extends string> =
-  S extends `${infer P1} ${infer P2}${infer P3}`
-    ? `${Lowercase<P1>}${Uppercase<P2>}${SentenceToCamelCase<P3>}`
-    : Lowercase<S>;
+// export type SentenceToCamelCase<S extends string> =
+//   S extends `${infer P1} ${infer P2}${infer P3}`
+//     ? `${Lowercase<P1>}${Uppercase<P2>}${SentenceToCamelCase<P3>}`
+//     : Lowercase<S>;
+
+// export type SentenceCaseKeysToCamelCase<T> = {
+//   [K in keyof T as SentenceToCamelCase<string & K>]: T[K] extends {}
+//     ? SentenceCaseKeysToCamelCase<T[K]>
+//     : T[K];
+// };
 
 export type SentenceCaseKeysToCamelCase<T> = {
   [K in keyof T as SentenceToCamelCase<string & K>]: T[K] extends {}
     ? SentenceCaseKeysToCamelCase<T[K]>
     : T[K];
 };
+
+export type SentenceToCamelCase<S extends string> =
+  S extends Uppercase<S>
+    ? Lowercase<S>
+    : S extends `${infer P1} ${infer P2}${infer P3}`
+      ? `${Lowercase<P1>}${Uppercase<P2>}${SentenceToCamelCase<P3>}`
+      : Lowercase<S>;
--- src/utils/camelCase.ts
+++ src/utils/camelCase.ts
@@ -1,8 +1,25 @@
 import { SentenceToCamelCase } from "@/types/camelCase";
 
+// export const sentenceToCamelCase = <T extends string>(
+//   str: T
+// ): SentenceToCamelCase<T> => {
+//   const result = str
+//     .replace(/(?:^\w|[A-Z]|\b\w)/g, (word: string, index: number) =>
+//       index === 0 ? word.toLowerCase() : word.toUpperCase()
+//     )
+//     .replace(/\s+/g, "");
+
+//   return result as SentenceToCamelCase<T>;
+// };
+
 export const sentenceToCamelCase = <T extends string>(
   str: T
 ): SentenceToCamelCase<T> => {
+  const isAllUppercase = str === str.toUpperCase();
+  if (isAllUppercase) {
+    return str.toLowerCase() as SentenceToCamelCase<T>;
+  }
+
   const result = str
     .replace(/(?:^\w|[A-Z]|\b\w)/g, (word: string, index: number) =>
       index === 0 ? word.toLowerCase() : word.toUpperCase()
